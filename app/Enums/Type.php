2f07d9b56c41ab64d5d95f3721da8792fc2da05d
Owen Voke <development@voke.dev>
2026-04-24 16:16:58 +0100

    feat: update types to match schema

--- app/Enums/Type.php
+++ app/Enums/Type.php
@@ -10,13 +10,15 @@ enum Type: string
     case Concentric = 'Concentric';
     case Folly = 'Folly';
     case Fort = 'Fort';
-    case FragmentedRemains = 'Fragmented Remains';
+    case FortifiedManorHouse = 'Fortified Manor House';
     case Keepless = 'Keepless';
     case MotteAndBailey = 'Motte and Bailey';
     case Palace = 'Palace';
     case RoundKeep = 'Round Keep';
     case ShellKeep = 'Shell Keep';
     case SquareKeep = 'Square Keep';
+    case TowerHouse = 'Tower House';
+    case Unknown = 'Unknown';
 
     public function label(): string
     {
