8b914568563504890237ee131c2f83ed46239f45
Owen Voke <development@voke.dev>
2026-05-04 16:39:42 +0100

    feat: add Sentry integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use App\Services\OIDC\Exceptions\OIDCConfigurationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,5 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+
         $exceptions->render(fn (OIDCConfigurationException $exception) => redirect()->route('oidc.error'));
     })->create();
